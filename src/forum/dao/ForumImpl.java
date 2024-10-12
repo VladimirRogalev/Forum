d25b4af31bef3ed933602edfeee0d7207836f1c0
Vladimir <vladimirrogalev1@gmail.com>
2024-10-12 14:56:10 +0300

    added analyze the time complexity

--- src/forum/dao/ForumImpl.java
+++ src/forum/dao/ForumImpl.java
@@ -31,6 +31,8 @@ public class ForumImpl implements Forum {
 		postsByTitle = new Post[INITIAL_CAPACITY];
 	}
 
+	// O(n)
+
 	@Override
 	public boolean addPost(Post post) {
 		if (post == null || getPostById(post.getPostId()) != null) {
@@ -40,14 +42,15 @@ public class ForumImpl implements Forum {
 			posts = Arrays.copyOf(posts, posts.length * 2);
 			postsByTitle = Arrays.copyOf(postsByTitle, postsByTitle.length * 2);
 		}
-
+		// O(log(n))
 		int index = Arrays.binarySearch(posts, 0, size, post, comparator);
 		index = index >= 0 ? index : -index - 1;
 		System.arraycopy(posts, index, posts, index + 1, size - index);
 		posts[index] = post;
-
+		// O(log(n))
 		index = Arrays.binarySearch(postsByTitle, 0, size, post, comparatorByTitle);
 		index = index >= 0 ? index : -index - 1;
+		// O(n)
 		System.arraycopy(postsByTitle, index, postsByTitle, index + 1, size - index);
 		postsByTitle[index] = post;
 		size++;
@@ -55,21 +58,23 @@ public class ForumImpl implements Forum {
 
 	}
 
+// O(n)
 	@Override
 	public boolean removePost(int postId) {
 		int index = searchById(postId);
 		if (index < 0) {
 			return false;
 		}
+		// O(n)
 		System.arraycopy(posts, index + 1, posts, index, size - index - 1);
 		posts = Arrays.copyOf(posts, posts.length - 1);
-
+		// O(n)
 		for (int i = 0; i < size; i++) {
 			if (posts[i].getPostId() == postId) {
 				index = i;
 			}
 		}
-
+		// O(n)
 		System.arraycopy(postsByTitle, index + 1, postsByTitle, index, size - index - 1);
 		posts = Arrays.copyOf(postsByTitle, postsByTitle.length - 1);
 
@@ -77,6 +82,7 @@ public class ForumImpl implements Forum {
 		return true;
 	}
 
+//O(n)
 	@Override
 	public boolean updatePost(int postId, String content) {
 		int index = searchById(postId);
@@ -87,11 +93,13 @@ public class ForumImpl implements Forum {
 		return true;
 	}
 
+	// O (n)
 	@Override
 	public Post getPostById(int postId) {
 		int index = searchById(postId);
 		return index < 0 ? null : posts[index];
 	}
+	// O(n)
 
 	private int searchById(int postId) {
 		for (int i = 0; i < size; i++) {
@@ -101,20 +109,22 @@ public class ForumImpl implements Forum {
 		}
 		return -1;
 	}
-
+	//O(n) => O(log(n)) -> O(n)
 	@Override
 	public Post[] getPostsByAuthor(String author) {
 		Post pattern = new Post(Integer.MIN_VALUE, null, author, null);
 		pattern.setDate(LocalDateTime.MIN);
+		// O(log(n))
 		int from = -Arrays.binarySearch(posts, 0, size, pattern, comparator) - 1;
 		pattern = new Post(Integer.MAX_VALUE, null, author, null);
 		pattern.setDate(LocalDateTime.MAX);
+		// O(log(n))
 		int to = -Arrays.binarySearch(posts, 0, size, pattern, comparator) - 1;
-
+ // O(n)
 		return Arrays.copyOfRange(posts, from, to);
 
 	}
-
+	//O(n) => O(log(n)) -> O(n)
 	@Override
 	public Post[] getPostsByAuthor(String author, LocalDate dateFrom, LocalDate dateTo) {
 		Post pattern = new Post(Integer.MIN_VALUE, null, author, null);
@@ -126,7 +136,7 @@ public class ForumImpl implements Forum {
 
 		return Arrays.copyOfRange(posts, from, to);
 	}
-
+	//O(n) => O(log(n)) -> O(n)
 	@Override
 	public Post[] getPostsByTitle(String title) {
 		Post pattern = new Post(Integer.MIN_VALUE, title, null, null);
@@ -139,7 +149,7 @@ public class ForumImpl implements Forum {
 		return Arrays.copyOfRange(postsByTitle, from, to);
 
 	}
-
+	//O(n) => O(log(n)) -> O(n)
 	@Override
 	public Post[] getPostsByTitle(String title, LocalDate dateFrom, LocalDate dateTo) {
 		Post pattern = new Post(Integer.MIN_VALUE, title, null, null);
@@ -149,17 +159,15 @@ public class ForumImpl implements Forum {
 		pattern.setDate(LocalDateTime.of(dateTo, LocalTime.MAX));
 		int to = -Arrays.binarySearch(postsByTitle, 0, size, pattern, comparatorByTitle) - 1;
 
-
 		return Arrays.copyOfRange(postsByTitle, from, to);
 	}
-	
-
+	// O(1)
 	@Override
 	public int size() {
 
 		return size;
 	}
-
+ // O(n)
 	@Override
 	public void printPosts() {
 		for (int i = 0; i < posts.length; i++) {
